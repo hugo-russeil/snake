4a93a4ba92d80572ebeeef7538f12e34fa6653a8
hrusseil <hugo.russeil@etudiant.univ-lr.fr>
2024-03-11 15:57:39 +0100

    Improved the handling of user input

--- src/main.c
+++ src/main.c
@@ -16,22 +16,36 @@ void main() {
         
         switch(getch()){
             case KEY_UP:
-                if(snake->next != NULL && snake->dirY == 1) break; // prevent the snake from reversing into itself
-                snake->dirX = 0;                                   // if the snake is longer than 1 node
-                snake->dirY = -1;                                  // in which case it can reverse
+                // check if the user is trying to do a 180 (which is not allowed),
+                // or to move in the direction he is already moving (which is useless)
+                if((snake->next != NULL && snake->dirY == 1) || (snake->dirX == 0 && snake->dirY == -1)){
+                    flushinp(); // clear the input buffer to prevent the user from queuing up too many inputs
+                    break;      // which would result in latency in the resolution of the inputs
+                }
+                snake->dirX = 0;                                   
+                snake->dirY = -1;                                  
                 break;
             case KEY_DOWN:
-                if(snake->next != NULL && snake->dirY == -1) break;
+                if((snake->next != NULL && snake->dirY == -1) || (snake->dirX == 0 && snake->dirY == 1)){
+                    flushinp();
+                    break;
+                }
                 snake->dirX = 0;
                 snake->dirY = 1;
                 break;
             case KEY_LEFT:
-                if(snake->next != NULL && snake->dirX == 1) break;
+                if((snake->next != NULL && snake->dirX == 1) || (snake->dirX == -1 && snake->dirY == 0)){
+                    flushinp();
+                    break;
+                }
                 snake->dirX = -1;
                 snake->dirY = 0;
                 break;
             case KEY_RIGHT:
-                if(snake->next != NULL && snake->dirX == -1) break;
+                if((snake->next != NULL && snake->dirX == -1) || (snake->dirX == 1 && snake->dirY == 0)){
+                    flushinp();
+                    break;
+                }
                 snake->dirX = 1;
                 snake->dirY = 0;
                 break;
